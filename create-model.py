9f6094c2afd67a94b34dd57a013b697496dc57ca
Nick Todd <nick.todd@conygre.com>
2020-12-27 17:10:12 +0000

    Updated docker image name

--- create-model.py
+++ create-model.py
@@ -1,20 +1,47 @@
+
 import boto3
-import os
 import sagemaker
-from utils.utils_cifar import get_train_data_loader, get_test_data_loader, imshow, classes
+from sagemaker import get_execution_role
 
 client = boto3.client('sts')
 account = client.get_caller_identity()['Account']
-region = "eu-west-1"
-algorithm_name = 'cifar10-example'
+
+
+role = "arn:aws:iam::963778699255:role/service-role/AmazonSageMaker-ExecutionRole-20201209T094957" #get_execution_role()
+
+region = boto3.Session().region_name
+sagemaker_session = sagemaker.session.Session()
+bucket = sagemaker_session.default_bucket()
+prefix = 'custom-ml-image'
+
+print(region)
+print(role)
+print(bucket)
+
 # for now we will assume the tag is always the latest
-ecr_image =  account + ".dkr.ecr.eu-west-1.amazonaws.com/cifar10example:latest"
+ecr_image =  account + ".dkr.ecr.eu-west-1.amazonaws.com/custommlimage:latest"
 
 session = sagemaker.Session()
 
-trainloader = get_train_data_loader('/tmp/pytorch-example/cifar-10-data')
-testloader = get_test_data_loader('/tmp/pytorch-example/cifar-10-data')
+print(session.upload_data('dummy.csv', bucket, prefix + '/train'))
+print(session.upload_data('dummy.csv', bucket, prefix + '/val'))
 
-prefix = 'DEMO-pytorch-cifar10'
-WORK_DIRECTORY = '/tmp/pytorch-example/cifar-10-data'
-data_location = session.upload_data(WORK_DIRECTORY, key_prefix=prefix)
+
+est = sagemaker.estimator.Estimator(ecr_image,
+                                    role, 
+                                    train_instance_count=1, 
+                                    train_instance_type='local', # use local mode
+                                    #train_instance_type='ml.m5.xlarge',
+                                    base_job_name=prefix)
+
+
+est.set_hyperparameters(hp1='value1',
+                        hp2=300,
+                        hp3=0.001)
+
+train_config = sagemaker.session.s3_input('s3://{0}/{1}/train/'.format(bucket, prefix), content_type='text/csv')
+val_config = sagemaker.session.s3_input('s3://{0}/{1}/val/'.format(bucket, prefix), content_type='text/csv')
+
+est.fit({'train': train_config, 'validation': val_config })
+
+
